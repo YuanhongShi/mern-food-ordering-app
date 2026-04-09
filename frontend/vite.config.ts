46fe718e0d4502f7cf253847408c8d3e2b162013
YuanhongShi <yuanhongshi@YuanhongShis-Mac-mini.local>
2026-04-09 08:48:52 +0800

    end of setup for frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,15 @@
+import path from "path"
+import react from "@vitejs/plugin-react"
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import path from "path";
+// 如果有其他插件，也一起导入
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    // ... 其他插件
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
