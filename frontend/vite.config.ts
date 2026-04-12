a5e9037b9ec9ed972dce695895c94cffeb57f278
YuanhongShi <shiyuanhong@hotmail.com>
2026-04-12 08:10:38 +0800

    made mobile nav component

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,11 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-// 如果有其他插件，也一起导入
+
 
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(),
     // ... 其他插件
   ],
   resolve: {
